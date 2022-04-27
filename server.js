8c6e275663c0b25c0be9175504c75e32642efca0
jgoldb22 <91508961+jgoldb22@users.noreply.github.com>
2022-04-26 21:59:45 -0500

    hello

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
-const connectHistory = require('connect-history-api-fallback')
 app = express();
-app.use(connectHistory())
 app.use(serveStatic(path.join(__dirname, '/dist')));
 const port = process.env.PORT || 8080;
 app.listen(port);
