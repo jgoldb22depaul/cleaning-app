4850f4739af83d82529b157697967eddddbb2c49
jgoldb22 <91508961+jgoldb22@users.noreply.github.com>
2022-04-30 15:48:15 -0500

    pathing for local host

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios' 
 export default axios.create ({
-    baseURL: 'https://backendy.herokuapp.com'
+    //baseURL: 'https://backendy.herokuapp.com'
+    baseURL: 'http://localhost:4000'
 })
