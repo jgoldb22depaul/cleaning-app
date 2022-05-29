571e5e849dd211d5e937e901116274cb478006c3
Gabriella Hoover <81646861+gabriellamhoover@users.noreply.github.com>
2022-05-28 23:51:41 -0500

    added error messages on acc page

--- src/views/AccountPage.vue
+++ src/views/AccountPage.vue
@@ -5,8 +5,9 @@
 	<p class="relative top-5 text-1xl lg:text-3xl font-bold" :style="{color: '#ECA72C'}"> Change Account Password: </p>
 	<form @submit.prevent="CheckPassword">
       <div class="mt-10 mx-auto w-10/12 rounded-2xl flex flex-col text-gray-800 p-4 shadow-lg max-w-2xl" :style="{ backgroundColor: '#E9967A'}">
-        <label for="oldpassword" :style="{color: '#F8FFE5'}">Old Password</label>
-		<input type="text" id="new" required class="bg-gray-100 p-2 mb-4 outline-none" v-model="oldpassword" spellcheck="false" placeholder="current password">
+	  <span class="leading-1" v-if="wrongPass != null" :style="{backgroundColor: '#F8FFE5', color: '#FD3A4A'}"> {{wrongPass}}</span>
+        <label for="oldPassInput" :style="{color: '#F8FFE5'}">Old Password</label>
+		<input type="text" id="new" required class="bg-gray-100 p-2 mb-4 outline-none" v-model="oldPassInput" spellcheck="false" placeholder="current password">
 		<label for="newpassword" :style="{color: '#F8FFE5'}">New Password</label>
 		<input type="text" id="new" required class="bg-gray-100  p-2 mb-4 outline-none" v-model="newpassword" spellcheck="false" placeholder="new password">
 		<div class="items-center">
@@ -14,13 +15,12 @@
 		</div>
 	  </div>
     </form>
-	
+	<span class="leading-1" v-if="badPass != null" :style="{backgroundColor: '#F8FFE5', color: '#FD3A4A'}"> {{badPass}}</span>
 	 <div id="deleteUser">
 	 <br>
 		<button v-on:click="requestDeleteUser()" class = "rounded-xl py-4 px-8 font-bold cursor-pointer text-sm text-white ml-2" :style="{ backgroundColor: '#FD3A4A', color: '#F8FFE5'}">Delete Account</button>
  </div>
     </div>
-	<span class="leading-1" v-if="badPass != null" :style="{backgroundColor: '#F8FFE5', color: '#FD3A4A'}"> {{message}}</span>
 </template>
 
 <script> 
@@ -31,8 +31,8 @@ export default {
   components: {Header},
   data() {
     return {
-      username: '',
 	   oldpassword: '',
+	   oldPassInput: '',
 	  newpassword: '',
 	  badPass: null,
 	  wrongPass: null
@@ -41,10 +41,13 @@ export default {
   methods: {
   CheckPassword(){
 	 const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
-	 if(specialChars.test(this.newpassword)){
+	 if(specialChars.test(this.newpassword) && (this.oldPassInput == this.oldpassword)){
 	 this.changePassword()}
-	 else{
+	 else if (!specialChars.test(this.newpassword)) {
 	 this.badPass = 'Password must be at least 8 characters and include at least 1 special character'
+	 }
+	 else if (this.oldPassInput != this.oldpassword){
+	 this.wrongPass = 'Current password is not correct!'
 	 }
 	 
   },
@@ -87,6 +90,23 @@ export default {
   
   },
   mounted() {
+  console.log(localStorage.currentUser)
+	axios
+		.get('/accountpage', {
+			params: {
+				username: localStorage.currentUser
+			}})
+		 .then((resp) => {
+            console.log(resp.data[0]);
+				if(resp.data){
+				this.oldpassword = resp.data[0].password;
+				console.log(this.oldpassword);
+        }
+          }, (error) => {
+            console.log(error);
+          });
+			
+			
     if (localStorage.currentUser) {
       this.username = localStorage.currentUser;
     }
